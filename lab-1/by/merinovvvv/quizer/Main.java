09ab029057fa19c972ef897fcab613d9d0e9661e
Yaraslau <yar.merinov@mail.ru>
2024-10-17 23:30:45 +0300

    thow in main is removed

--- lab-1/by/merinovvvv/quizer/Main.java
+++ lab-1/by/merinovvvv/quizer/Main.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
 public class Main {
 
 
-    public static void main(String[] args) throws QuizNotFinishedException {
+    public static void main(String[] args) {
 
         //TODO make tests (quizzes) + GroupTaskGenerator, PoolTaskGenerator
         try {
